49cdfe74136babac56debbe32d35d9b3090aac27
thasneemmanaf <123thasneemsalim@gmail.com>
2020-09-09 22:59:01 +0200

    v1.0

---
+++ src/Actions.js
@@ -0,0 +1,301 @@
+import React, { useState, useEffect } from "react";
+// read: https://fontawesome.com/how-to-use/on-the-web/using-with/react
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import data from "./data.json";
+
+const Actions = (props) => {
+  const { setRestaurants } = props;
+
+  const [showSortSelector, setSortSelector] = useState(false);
+  const [showPriceRangeSelector, setPriceRangeSelector] = useState(false);
+  const [showDietaryChoiceSelector, setDietaryChoiceSelector] = useState(false);
+  const [dietarySelections, setDietarySelections] = useState([]);
+
+  // Show/hide sort selector button
+  const handleOnClickSortSelector = () => {
+    setSortSelector(!showSortSelector);
+    setPriceRangeSelector(false);
+    setDietaryChoiceSelector(false);
+  };
+
+  // Show/hide price range selector button
+  const handleOnClickPriceSelector = () => {
+    setPriceRangeSelector(!showPriceRangeSelector);
+    setSortSelector(false);
+    setDietaryChoiceSelector(false);
+  };
+
+  // Show/hide Dietary choice selector button
+  const handleOnClickDietarySelector = () => {
+    setDietaryChoiceSelector(!showDietaryChoiceSelector);
+    setPriceRangeSelector(false);
+    setSortSelector(false);
+  };
+
+  // Filter restaurants based on price range
+  const filterRestaurants = (
+    restaurants,
+    selectedMinPrice,
+    selectedMaxPrice
+  ) => {
+    let filteredRestaurants = [];
+    let filteredRestaurantIds = [];
+
+    // Get filtered restaurants and corresponding ids
+    restaurants.forEach((restaurant) => {
+      restaurant.menu.forEach((menuItem) => {
+        menuItem.items.forEach((item) => {
+          if (
+            item.price >= selectedMinPrice &&
+            item.price <= selectedMaxPrice
+          ) {
+            if (!filteredRestaurantIds.includes(restaurant.id)) {
+              filteredRestaurantIds.push(restaurant.id);
+              filteredRestaurants.push(restaurant);
+            }
+          }
+        });
+      });
+    });
+    setRestaurants(filteredRestaurants);
+  };
+
+  // Filter restaurants based on Dietary choice
+  const filterRestaurantsDietary = (restaurants, dietarySelections) => {
+    restaurants = data.restarants;
+    let filteredRestaurants = [];
+    let filteredRestaurantIds = [];
+    // Get filtered restaurants and corresponding ids
+    restaurants.forEach((restaurant) => {
+      restaurant.menu.forEach((menuItem) => {
+        menuItem.items.forEach((item) => {
+          if (
+            dietarySelections.includes(item.typeOfMeal) &&
+            !filteredRestaurantIds.includes(restaurant.id)
+          ) {
+            filteredRestaurantIds.push(restaurant.id);
+            filteredRestaurants.push(restaurant);
+          }
+        });
+      });
+    });
+    console.log(filteredRestaurants);
+    console.log(filteredRestaurantIds);
+    setRestaurants(filteredRestaurants);
+  };
+
+  // Get min and max prices based on buttons($, $$, $$$, $$$$)
+  const getMinAndMaxPrice = (priceRange) => {
+    let selectedMaxPrice;
+    let selectedMinPrice;
+    if (priceRange === "$") {
+      selectedMinPrice = 0;
+      selectedMaxPrice = 100;
+    } else if (priceRange === "$$") {
+      selectedMinPrice = 100;
+      selectedMaxPrice = 200;
+    } else if (priceRange === "$$$") {
+      selectedMinPrice = 200;
+      selectedMaxPrice = 250;
+    } else if (priceRange === "$$$$") {
+      selectedMinPrice = 250;
+      selectedMaxPrice = 300;
+    } else {
+      throw new Error("Incorrect selection");
+    }
+    return [selectedMinPrice, selectedMaxPrice];
+  };
+
+  //  Sort button: To sort restaurants based on popularity,rating and delivery time
+  const handeGeneralSorting = (restaurants, sortBy) => {
+    let func;
+    if (sortBy === "maxDeliveryTime") {
+      func = (a, b) => a[sortBy] - b[sortBy]; // not that easy?
+    } else {
+      func = (a, b) => b[sortBy] - a[sortBy];
+    }
+    setRestaurants([...restaurants].sort(func));
+  };
+
+  //  Dietary Choice button: To filter restaurants based on vegetarian,vegan and non vegan
+  const handleDietaryFiltering = (restaurants, selection) => {
+    const newSelection = [...dietarySelections];
+    // Add items when it is checked  and remove items when unchecked
+    if (selection.checked) {
+      newSelection.push(selection.value);
+    } else {
+      const index = newSelection.indexOf(selection.value);
+      if (index > -1) {
+        newSelection.splice(index, 1);
+      }
+    }
+    setDietarySelections(newSelection);
+    filterRestaurantsDietary(restaurants, newSelection);
+  };
+
+  //  Price Range button: To filter restaurants based on price range
+  const handlePriceFiltering = (restaurants, priceRange) => {
+    restaurants = data.restarants;
+    const [selectedMinPrice, selectedMaxPrice] = getMinAndMaxPrice(priceRange);
+    filterRestaurants(restaurants, selectedMinPrice, selectedMaxPrice);
+  };
+
+  return (
+    <div className="actions">
+      {/* Sort button */}
+      <span className="button-group">
+        <button onClick={handleOnClickSortSelector}>
+          <span>Sort</span> <FontAwesomeIcon icon={"chevron-down"} />
+        </button>
+        {showSortSelector && (
+          <div>
+            <label>
+              Most popular{" "}
+              <input
+                value="popularity"
+                type="radio"
+                name="generalSort"
+                onChange={(event) =>
+                  handeGeneralSorting(
+                    props.restaurants,
+                    event.currentTarget.value
+                  )
+                }
+              />
+            </label>
+            <label>
+              Rating{" "}
+              <input
+                value="rating"
+                type="radio"
+                name="generalSort"
+                onChange={(event) =>
+                  handeGeneralSorting(
+                    props.restaurants,
+                    event.currentTarget.value
+                  )
+                }
+              />
+            </label>
+            <label>
+              Delivery time{" "}
+              <input
+                value="maxDeliveryTime"
+                type="radio"
+                name="generalSort"
+                onChange={(event) =>
+                  handeGeneralSorting(
+                    props.restaurants,
+                    event.currentTarget.value
+                  )
+                }
+              />
+            </label>
+          </div>
+        )}
+      </span>
+      {/* TODO */}
+      {/* <!-- Implement as assignment for Thursday --> */}
+
+      {/* Price range button */}
+      <span className="button-group">
+        <button onClick={handleOnClickPriceSelector}>
+          <span>Price Range</span> <FontAwesomeIcon icon={"chevron-down"} />
+        </button>
+        {showPriceRangeSelector && (
+          <div className="buttons">
+            <button
+              value="$"
+              onClick={(event) =>
+                handlePriceFiltering(
+                  props.restaurants,
+                  event.currentTarget.value
+                )
+              }
+            >
+              $
+            </button>
+            <button
+              value="$$"
+              onClick={(event) =>
+                handlePriceFiltering(
+                  props.restaurants,
+                  event.currentTarget.value
+                )
+              }
+            >
+              $$
+            </button>
+            <button
+              value="$$$"
+              onClick={(event) =>
+                handlePriceFiltering(
+                  props.restaurants,
+                  event.currentTarget.value
+                )
+              }
+            >
+              $$$
+            </button>
+            <button
+              value="$$$$"
+              onClick={(event) =>
+                handlePriceFiltering(
+                  props.restaurants,
+                  event.currentTarget.value
+                )
+              }
+            >
+              $$$$
+            </button>
+          </div>
+        )}
+      </span>
+
+      {/* Dietary choice button */}
+      <span className="button-group">
+        <button onClick={handleOnClickDietarySelector}>
+          <span>Dietary choice</span> <FontAwesomeIcon icon={"chevron-down"} />
+        </button>
+        {showDietaryChoiceSelector && (
+          <div>
+            <label>
+              Vegetarian
+              <input
+                value="vegetarian"
+                type="checkbox"
+                name="generalFilter"
+                onChange={(event) =>
+                  handleDietaryFiltering(props.restaurants, event.currentTarget)
+                }
+              />
+            </label>
+            <label>
+              Vegan
+              <input
+                value="vegan"
+                type="checkbox"
+                name="generalFilter"
+                onChange={(event) =>
+                  handleDietaryFiltering(props.restaurants, event.currentTarget)
+                }
+              />
+            </label>
+            <label>
+              Non-Vegan
+              <input
+                value="non vegan"
+                type="checkbox"
+                name="generalFilter"
+                onChange={(event) =>
+                  handleDietaryFiltering(props.restaurants, event.currentTarget)
+                }
+              />
+            </label>
+          </div>
+        )}
+      </span>
+    </div>
+  );
+};
+export default Actions;
---
+++ src/App.js
@@ -0,0 +1,61 @@
+import "./App.css";
+import React, { useState, useCallback } from "react";
+import Actions from "./Actions";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import data from "./data.json";
+
+library.add(faChevronDown);
+
+function App() {
+  const [restaurants, setRestaurants] = useState([]);
+
+  // Explore effect! (for classfull, this was related to componentWillMount, componentDidMount side effects) )
+  React.useEffect(() => {
+    setRestaurants(data.restarants);
+  }, []);
+
+  const [restuarantName, setName] = useState("");
+
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <a href="/">
+            SC<span style={{ color: "#06c167" }}>eats</span>
+          </a>
+        </nav>
+        <Actions setRestaurants={setRestaurants} restaurants={restaurants} />
+        <form className="searchFood" onSubmit={(e) => e.preventDefault()}>
+          <input
+            type="text"
+            placeholder="Search for food"
+            value={restuarantName}
+            onBlur={(e) => setName(e.target.value)}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              // TODO - future implementation
+            }}
+          >
+            Search
+          </button>
+        </form>
+      </header>
+      <div className="restaurants">
+        {restaurants.map((restaurant) => {
+          return (
+            <section key={restaurant.id}>
+              <img src={restaurant.img} alt="" />
+              <h2>{restaurant.name}</h2>
+              <p className="description">{restaurant.description}</p>
+            </section>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
